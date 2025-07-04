bd9b59dc1aec9303007762150492a31ee55aecb2
piyapawn <clasperqueen@gmail.com>
2025-07-05 01:23:45 +0700

    Setting config
    setting config to netlify deploy error and remove unuse svg

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
   async redirects() {
     return [
       {
         source: '/',
         destination: '/home-page',
-        permanent: true
-      }
-    ]
+        permanent: true,
+      },
+    ];
   },
 };
 
